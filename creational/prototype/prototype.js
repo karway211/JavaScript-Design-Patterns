fc72a6a9dd0a8f3805e46ac8a92161ee42231dd8
Andrey Karcheuski <karway211@gmail.com>
2020-06-27 01:19:58 +0300

    feat: add design pattern prototype

---
+++ creational/prototype/prototype.js
@@ -0,0 +1,29 @@
+/*
+prototype - это паттерн, с помощью которого мы можем создать копию объекта везде, где нам это потребуется с минимальными затратами
+памяти, т.к. создается копия на основании уже существующей структуры. И, в случае необходимости, модифицировать каждый экземпляр точечно
+под определенные нужды не изменяя базовой структуры.
+*/
+
+class TeslaCar {
+
+	constructor(model, price, interior, autopilot) {
+		this.model = model;
+		this.price = price;
+		this.interior = interior;
+		this.autopilot = autopilot;
+	}
+
+	produce() {
+		return new TeslaCar(this.model, this.price, this.interior, this.autopilot);
+	}
+};
+
+const prototypeCar = new TeslaCar('S', 80000, 'black', false);
+
+const car1 = prototypeCar.produce();
+const car2 = prototypeCar.produce();
+
+car1.interior = 'white';
+car1.autopilot = 'true';
+car2.interior = 'black';
+car2.autopilot = 'false';
